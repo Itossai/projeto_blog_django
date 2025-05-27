ab63d5da821eb398979a8ab371ab7ec7b2034cb7
Itossai <dantedeleon.ti@gmail.com>
2025-05-27 17:07:38 -0300

    adicioanndo imageFields no Site_Setup

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.urls import path
 from blog.views import index
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = 'blog'
 
 urlpatterns = [
     path('', index,name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
