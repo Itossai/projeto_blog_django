59d88707d4f98f47f07440c688bd89223178965a
Ittossai <dantedeleon.ti@gmail.com>
2025-08-01 22:36:04 -0300

    adicionado slug no html e alterando postcards, adicionando postmanager contente

--- blog/urls.py
+++ blog/urls.py
@@ -23,7 +23,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', index,name='index'),
     path('page/', page,name='page'),
-    path('post/', post,name='post'),
+    path('post/<slug:slug>/', post,name='post'),
 ]
 
 if settings.DEBUG:
--- blog/views.py
+++ blog/views.py
@@ -1,41 +1,45 @@
+from blog.models import Post
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
-posts = list(range(1000))
-# Create your views here.
+PER_PAGE = 9
+
 
 def index(request):
-    paginator = Paginator(posts,9)
-    page_number = request.GET.get('page')
+    posts = (
+        Post
+        .objects
+        .get_published()
+                )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
+
     return render(
         request,
         'blog/pages/index.html',
         {
-            'page_obj':page_obj,
+            'page_obj': page_obj,
         }
     )
 
+
 def page(request):
-    paginator = Paginator(posts,9)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     return render(
         request,
         'blog/pages/page.html',
         {
-            'page_obj':page_obj,
+            # 'page_obj': page_obj,
         }
     )
 
+
 def post(request):
-    paginator = Paginator(posts,9)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     return render(
         request,
         'blog/pages/post.html',
         {
-            'page_obj':page_obj,
+            # 'page_obj': page_obj,
         }
-    )   
+    )
