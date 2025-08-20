b50efec7b8f17e036617c5735cd65b0295aea5dd
Ittossai <dantedeleon.ti@gmail.com>
2025-08-19 21:23:11 -0300

    adicionando tags many to many no template do blog

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from blog.views import index,page,post, created_by,category
+from blog.views import index,page,post, created_by,category,tag
 from django.conf import settings
 from django.conf.urls.static import static
 app_name = 'blog'
@@ -26,7 +26,7 @@ urlpatterns = [
     path('post/<slug:slug>/', post,name='post'),
     path('category/<slug:slug>/',category, name='category'),
     path('created_by/<int:author_pk>/',created_by, name='created_by'),
-    
+    path('tag/<slug:slug>/',tag, name='tag'),
 ]
 
 if settings.DEBUG:
