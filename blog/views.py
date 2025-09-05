a745f1797af6b748274717d09df1d8cd1b3ac809
Ittossai <dantedeleon.ti@gmail.com>
2025-09-05 09:41:27 -0300

    migrando search para SearcViewList

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from django.db.models.query import QuerySet
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import Http404
 from django.views.generic.list import ListView
 from typing import Any
@@ -140,28 +140,8 @@ class CategoryListView(PostListView):
 
 
 
-def tag(request,slug):
-    posts = Post.objects.get_published()\
-    .filter(tags__slug=slug)
-    paginator = Paginator(posts,PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
 
-    if len(page_obj) == 0:
-        raise Http404()
-
-    page_title = f'{page_obj[0].tags.first().name} - Tag -'
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj':page_obj,
-            'page_title':page_title
-        }
-    )
-
-class TagByListView(PostListView):
+class TagListView(PostListView):
     allow_empty = False
 
     def get_queryset(self) -> QuerySet[Any]:
@@ -203,3 +183,42 @@ def search(request):
             'page_title':page_title
         }
     )
+
+class SearchListView(PostListView):
+    
+    
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self._search_value = ''
+
+    def setup(self, request, *args, **kwargs):
+        self._search_value = request.GET.get('search','').strip()
+
+        return super().setup(request, *args, **kwargs)
+
+ 
+    def get_queryset(self) -> QuerySet[Any]:
+        search_value = self._search_value
+        return super().get_queryset().filter(
+        Q(title__icontains=search_value) |
+        Q(excerpt__icontains=search_value) |
+        Q(content__icontains=search_value)
+        )[:PER_PAGE]
+        
+    
+    def get_context_data(self,**kwargs):
+        ctx = super().get_context_data(**kwargs)
+        search_value = self._search_value
+        page_title = f'{search_value[:30]} - Search -'
+        ctx.update(
+            {
+                'page_title':page_title,
+                'search_value':search_value
+            }
+        )
+        return ctx 
+
+    def get(self, request, *args, **kwargs):
+        if self._search_value == '':
+            return redirect('blog:index')
+        return super().get(request, *args, **kwargs)
